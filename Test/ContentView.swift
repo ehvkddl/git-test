f01b4ba1f8e0fa6b4d03403d3fb409ed22427c3d
ehvkddl <ehvkddl@naver.com>
2022-12-02 10:42:42 +0900

    Revert "first commit"
    This reverts commit c7f573a125d8d8a6acb0a02d6e6e7d3510beaa00.

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "figure.walk")
+            Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
